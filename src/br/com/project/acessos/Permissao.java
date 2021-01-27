4be31beb269d6defda786de44e0eb0908522ab84
Leandro Amorim <leandroamorimpb@gmail.com>
2021-01-26 23:47:33 -0300

    Enum de permissão de acessos

--- src/br/com/project/acessos/Permissao.java
+++ src/br/com/project/acessos/Permissao.java
@@ -1,5 +1,10 @@
 package br.com.project.acessos;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public enum Permissao {
 	
 	ADMIN("ADMIN", "ADMINISTRADOR"),
@@ -43,6 +48,25 @@ public enum Permissao {
 	@Override
 	public String toString() {
 		return getValor();
+	}
+	
+	public static List<Permissao> getListPermissao(){
+		List<Permissao> permissoes = new ArrayList<Permissao>();
+		
+		for (Permissao permissao : Permissao.values()) {
+			permissoes.add(permissao);
+		}
+		
+		Collections.sort(permissoes, new Comparator<Permissao>() {
+
+			@Override
+			public int compare(Permissao o1, Permissao o2) {
+				return new Integer(o1.ordinal()).compareTo(new Integer(o2.ordinal()));
+			}
+			
+		});
+		
+		return permissoes;
 	}
 
 }
